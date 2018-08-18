b0ecd567d7b00a4482925b43f50c80743635b9df
umarolt <umarolt@firstblood.io>
2018-08-18 15:54:22 +0200

    Resolving and parsing expressions

--- utils/src/main/kotlin/me/marolt/configurationserver/utils/ParserUtils.kt
+++ utils/src/main/kotlin/me/marolt/configurationserver/utils/ParserUtils.kt
@@ -11,25 +11,101 @@ internal fun parseVariable(currentKey: String, allProperties: Map<String, String
     TODO()
 }
 
-internal fun resolveVariables(value: String): List<String> {
-    val variables = mutableListOf<String>()
+internal fun resolveExpressions(value: String): Map<String, Expression> {
+    val expressions = mutableMapOf<String, Expression>()
 
     var openIndex = -1
     var currentIndex = 0
-    while (currentIndex < value.length ) {
+    while (currentIndex < value.length) {
 
         if (value[currentIndex] == '{' && currentIndex > 0 && value[currentIndex - 1] == '$' &&
                 (currentIndex - 1 == 0 || value[currentIndex - 2] != '\\')) { // ignore escaped values
             openIndex = currentIndex + 1
         }
 
-        if(value[currentIndex] == '}' && openIndex > 0) {
-            variables.add(value.substring(openIndex, currentIndex))
+        if (value[currentIndex] == '}' && openIndex > 0) {
+            val expression = value.substring(openIndex, currentIndex).trim()
+            expressions["\${$expression}"] = parseExpression(expression)
             openIndex = -1
         }
 
         ++currentIndex
     }
 
-    return variables
+    return expressions
 }
+
+private val CONST_REGEX = "^\\d+(\\.\\d+)?".toRegex()
+
+internal fun parseExpression(expr: String): Expression {
+    if (expr.matches(CONST_REGEX)) {
+        return Constant(expr.toDouble())
+    }
+
+    val bracketStack = Stack<Int>()
+    var firstExpression: Expression? = null
+
+    for (i in 0 until expr.length) {
+        val character = expr[i]
+
+        if (!bracketStack.empty() && character != '(' && character != ')') continue
+
+        when (character) {
+            '(' -> {
+                bracketStack.push(i)
+            }
+            ')' -> {
+                val openBracket = bracketStack.pop()
+                if (bracketStack.empty()) {
+                    firstExpression = parseExpression(expr.substring(openBracket + 1, i))
+                }
+            }
+            '+' -> return Sum(firstExpression ?: parseExpression(expr.substring(0, i).trim()),
+                    parseExpression(expr.substring(i + 1).trim()))
+            '-' -> return Sub(firstExpression ?: parseExpression(expr.substring(0, i).trim()),
+                    parseExpression(expr.substring(i + 1).trim()))
+            '*' -> return Mul(firstExpression ?: parseExpression(expr.substring(0, i).trim()),
+                    parseExpression(expr.substring(i + 1).trim()))
+            '/' -> return Div(firstExpression ?: parseExpression(expr.substring(0, i).trim()),
+                    parseExpression(expr.substring(i + 1).trim()))
+            else -> {
+            }
+        }
+    }
+
+    if (firstExpression == null) return Variable(expr)
+
+    return firstExpression
+}
+
+internal fun unresolvedVariables(expr: Expression, variables: Set<String>): Set<String> {
+    when (expr) {
+        is Constant -> return emptySet()
+        is Variable -> {
+            return if (variables.contains(expr.name)) {
+                emptySet()
+            } else {
+                setOf(expr.name)
+            }
+        }
+        is TwoOperandsOperation -> {
+            val a = unresolvedVariables(expr.a, variables)
+            val b = unresolvedVariables(expr.b, variables)
+
+            return a.union(b)
+        }
+    }
+}
+
+internal fun evaluateExpression(expr: Expression, variables: Map<String, Double>): Double {
+    TODO()
+}
+
+internal sealed class Expression
+internal data class Constant(val number: Double) : Expression()
+internal data class Variable(val name: String) : Expression()
+internal sealed class TwoOperandsOperation(open val a: Expression, open val b: Expression) : Expression()
+internal data class Sum(override val a: Expression, override val b: Expression) : TwoOperandsOperation(a, b)
+internal data class Sub(override val a: Expression, override val b: Expression) : TwoOperandsOperation(a, b)
+internal data class Mul(override val a: Expression, override val b: Expression) : TwoOperandsOperation(a, b)
+internal data class Div(override val a: Expression, override val b: Expression) : TwoOperandsOperation(a, b)
--- utils/src/test/kotlin/me/marolt/configurationserver/utils/ParserUtilsTests.kt
+++ utils/src/test/kotlin/me/marolt/configurationserver/utils/ParserUtilsTests.kt
@@ -1,42 +1,245 @@
 package me.marolt.configurationserver.utils
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserUtilsTests {
 
-    @Test
-    @DisplayName("Resolve single variable in a string")
-    fun resolve_single_variable() {
-        val value = "test123\${test.test1.test2}"
-        val results = resolveVariables(value)
+    @Nested
+    @DisplayName("Resolving expressions")
+    inner class ExpressionResolvingTests {
+        @Test
+        @DisplayName("Resolve single expression in a string")
+        fun resolve_single_expression() {
+            val value = "test123\${test.test1.test2}"
+            val results = resolveExpressions(value)
 
-        assertEquals(results.size, 1)
-        assertEquals(results[0], "test.test1.test2")
+            assertEquals(1, results.size)
+            val entries = results.entries.toTypedArray()
+            assertEquals("\${test.test1.test2}", entries[0].key)
+            assertEquals(Variable("test.test1.test2"), entries[0].value)
+        }
+
+        @Test
+        @DisplayName("Resolve multiple expressions in a string")
+        fun resolve_multiple_expressions() {
+            val value = "test1\${test.test1}test2\${test.test2}test3\${test.test3}test4"
+            val results = resolveExpressions(value)
+
+            assertEquals(3, results.size)
+            val entries = results.entries.toTypedArray()
+            assertEquals("\${test.test1}", entries[0].key)
+            assertEquals(Variable("test.test1"), entries[0].value)
+
+            assertEquals("\${test.test2}", entries[1].key)
+            assertEquals(Variable("test.test2"), entries[1].value)
+
+            assertEquals("\${test.test3}", entries[2].key)
+            assertEquals(Variable("test.test3"), entries[2].value)
+        }
+
+        @Test
+        @DisplayName("Ignore escaped expressions")
+        fun ignore_escaped_expressions() {
+            val value = "test1\${test.test1}test2\\\${test.test2}test3\\\${test.test3}test4"
+            val results = resolveExpressions(value)
+
+            assertEquals(1, results.size)
+            val entries = results.entries.toTypedArray()
+            assertEquals("\${test.test1}", entries[0].key)
+            assertEquals(Variable("test.test1"), entries[0].value)
+        }
     }
 
-    @Test
-    @DisplayName("Resolve multiple variables in a string")
-    fun resolve_multiple_variables() {
-        val value = "test1\${test.test1}test2\${test.test2}test3\${test.test3}test4"
-        val results = resolveVariables(value)
+    @Nested
+    @DisplayName("Parsing expressions")
+    inner class ExpressionParsingTests {
+        @Test
+        @DisplayName("Parse integer constant")
+        fun parse_integer_constant() {
+            val value = "123"
+            val result = parseExpression(value)
 
-        assertEquals(results.size, 3)
-        assertEquals(results[0], "test.test1")
-        assertEquals(results[1], "test.test2")
-        assertEquals(results[2], "test.test3")
+            assertEquals(Constant(123.0), result)
+        }
+
+        @Test
+        @DisplayName("Parse double constant")
+        fun parse_double_constant() {
+            val value = "123.456"
+            val result = parseExpression(value)
+
+            assertEquals(Constant(123.456), result)
+        }
+
+        @Test
+        @DisplayName("Parse variable")
+        fun parse_variable() {
+            val value = "test123"
+            val result = parseExpression(value)
+
+            assertEquals(Variable("test123"), result)
+        }
+
+        @Test
+        @DisplayName("Parse addition")
+        fun parse_sum() {
+            val value = "test123+ 123.456"
+            val result = parseExpression(value)
+
+            assertEquals(Sum(Variable("test123"), Constant(123.456)), result)
+        }
+
+        @Test
+        @DisplayName("Parse subtraction")
+        fun parse_sub() {
+            val value = "test123 - 123.456"
+            val result = parseExpression(value)
+
+            assertEquals(Sub(Variable("test123"), Constant(123.456)), result)
+        }
+
+        @Test
+        @DisplayName("Parse multiplication")
+        fun parse_mul() {
+            val value = "test123 *123.456"
+            val result = parseExpression(value)
+
+            assertEquals(Mul(Variable("test123"), Constant(123.456)), result)
+        }
+
+        @Test
+        @DisplayName("Parse division")
+        fun parse_div() {
+            val value = "test123 / 123.456"
+            val result = parseExpression(value)
+
+            assertEquals(Div(Variable("test123"), Constant(123.456)), result)
+        }
+
+        @Test
+        @DisplayName("Parse complicated expression")
+        fun parse_complicated_expression() {
+            val value = "test123 * 123.456 - test456 / 789.123"
+            val result = parseExpression(value)
+
+            assertEquals(
+                    Mul(
+                            Variable("test123"),
+                            Sub(
+                                    Constant(123.456),
+                                    Div(
+                                            Variable("test456"),
+                                            Constant(789.123)
+                                    )
+                            )
+                    ),
+                    result
+            )
+        }
+
+        @Test
+        @DisplayName("Parse simple expression with enclosing brackets")
+        fun parse_with_brackets() {
+            val value = "(test123 * 123.456)"
+            val result = parseExpression(value)
+
+            assertEquals(Mul(Variable("test123"), Constant(123.456)), result)
+        }
+
+        @Test
+        @DisplayName("Parse expression with multiple brackets")
+        fun parse_with_multiple_brackets() {
+            val value = "(test123 * (test456 + 123.456))"
+            val result = parseExpression(value)
+
+            assertEquals(
+                    Mul(
+                            Variable("test123"),
+                            Sum(
+                                    Variable("test456"),
+                                    Constant(123.456)
+                            )
+                    ),
+                    result
+            )
+        }
+
+        @Test
+        @DisplayName("Parse complicated expression with brackets")
+        fun parse_complicated_expression_with_brackets() {
+            val value = "((test123 * 123.456) - test456) / 789.123"
+            val result = parseExpression(value)
+            println(result)
+
+            assertEquals(
+                    Div(
+                            Sub(
+                                    Mul(
+                                            Variable("test123"),
+                                            Constant(123.456)),
+                                    Variable("test456")),
+                            Constant(789.123)
+                    ),
+                    result)
+        }
     }
 
-    @Test
-    @DisplayName("Ignore escaped variables")
-    fun ignore_escaped_variables() {
-        val value = "test1\${test.test1}test2\\\${test.test2}test3\\\${test.test3}test4"
-        val results = resolveVariables(value)
+    @Nested
+    @DisplayName("Evaluating expressions")
+    inner class ExpressionEvaluatingTests {
 
-        assertEquals(results.size, 1)
-        assertEquals(results[0], "test.test1")
+        @Test
+        @DisplayName("Detect unresolved variables")
+        fun detect_unresolved_variables() {
+            val expr = Mul(
+                    Variable("test123"),
+                    Sub(
+                            Constant(123.456),
+                            Div(
+                                    Variable("test456"),
+                                    Sum(
+                                            Constant(789.0),
+                                            Variable("test789")
+                                    )
+                            )
+                    )
+            )
+            val resolvedVariables = setOf("test123")
+
+            val unresolvedVariables = unresolvedVariables(expr, resolvedVariables)
+
+            assertEquals(2, unresolvedVariables.size)
+
+            assertTrue(unresolvedVariables.contains("test456"))
+            assertTrue(unresolvedVariables.contains("test789"))
+        }
+
+        @Test
+        @DisplayName("Detect no unresolved variables")
+        fun detect_no_unresolved_variables() {
+            val expr = Mul(
+                    Variable("test123"),
+                    Sub(
+                            Constant(123.456),
+                            Div(
+                                    Variable("test456"),
+                                    Sum(
+                                            Constant(789.0),
+                                            Variable("test789")
+                                    )
+                            )
+                    )
+            )
+            val resolvedVariables = setOf("test123", "test456", "test789")
+            val unresolvedVariables = unresolvedVariables(expr, resolvedVariables)
+
+            assertEquals(0, unresolvedVariables.size)
+        }
+
     }
+
 }
